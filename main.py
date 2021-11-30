1d2d4a6ef142ff0798f6a4a044f7e35f49e57602
fengelmann <github@fengelmann.de>
2021-11-30 09:08:54 +0100

    Convert to png

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import os
 
-from flask import Flask
+from flask import Flask, send_file
+from cairosvg import svg2png
 
 app = Flask(__name__)
 
 
 @app.route("/<smiles>", methods=["GET"])
-def map(smiles):
+def convert_smiles_to_molecule_image(smiles):
     os.system('obabel -:"{}" -O molecule.svg'.format(smiles))
-    return open("molecule.svg", "r").read()
+    svg = open("molecule.svg", "rb")
+    svg2png(file_obj=svg, write_to='molecule.png')
+    return send_file('molecule.png', mimetype='image/png')
 
 
 if __name__ == '__main__':
